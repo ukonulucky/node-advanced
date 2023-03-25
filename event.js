7d8bf5a4afc633976805bdcd0110a4b3b28f8fe8
ukonulucky <ukonulucky@gmail.com>
2023-03-25 20:04:16 +0100

    node advanced

---
+++ event.js
@@ -0,0 +1,28 @@
+const EventEmiter = require('events')
+
+const celebrity = new  EventEmiter()
+
+
+// fan 1 subscribing to the events
+
+celebrity.on("won race", (data) => {
+   if(data === "gold"){
+       console.log("Congratulations bro for winning ", data)
+   }
+})
+
+
+// fan 2 subscribing to the events
+celebrity.on("won race", (data) => {
+    if(data === "bronze"){
+        console.log("Congratulations bro for winning ", data)
+    }
+})
+
+
+process.on("exit", (code) => {
+    console.log("code ended with status code", code)
+})
+
+celebrity.emit("won race","gold")
+celebrity.emit("won race","bronze")
